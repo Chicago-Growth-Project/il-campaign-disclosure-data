bd855ce8172b290d1110294e23573270a96a6879
Franco Reyes <incazteca12345@gmail.com>
2025-09-13 14:17:26 -0500

    table test wip

--- table_test.go
+++ table_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
@@ -24,4 +27,34 @@ func TestIndexColumnSQLs(t *testing.T) {
 			t.Errorf("Expected %s, got %s", expected, actual[i])
 		}
 	}
+}
+
+func TestCreateWWindows1252file(t *testing.T) {
+	tServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/csv; charset=windows-1252")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ID, Year, Name\n"))
+		w.Write([]byte("1,2023,John Doe\n"))
+		w.Write([]byte("2,2023,Jane Smith\n"))
+		w.Write([]byte("3,2023,Emily Davis\n"))
+		w.Write([]byte("4,       ,Franco Reyes\n"))
+		w.Write([]byte("05,       ,Michael Brown\n"))
+		w.Write([]byte("06,       ,Chris Johnson\n"))
+		w.Write([]byte("7,\"2021\",Connor O'Neil\n"))
+	}))
+
+	defer tServer.Close()
+
+	testTable := Table{
+		Name:     "test_table",
+		URL:      "http://example.com/test_file.csv",
+		FileType: CSV,
+		Columns: []Column{
+			{Name: "id", RawName: "ID", Type: ColumnTypeInt, NotNullable: true},
+			{Name: "year", RawName: "Year", Type: ColumnTypeInt, NotNullable: true},
+			{Name: "name", RawName: "Name", Type: ColumnTypeString},
+		},
+	}
+
+	fmt.Println("test table: ", testTable)
 }
